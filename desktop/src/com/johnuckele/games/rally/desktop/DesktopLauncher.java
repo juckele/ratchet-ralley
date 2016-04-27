c4beec9b4668d2ab312331ef8c14e602f032d38d
John H. Uckele <j@vivarium.io>
2016-04-27 18:27:43 -0400

    First Commit

---
+++ desktop/src/com/johnuckele/games/rally/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.johnuckele.games.rally.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.johnuckele.games.rally.RatchetRally;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RatchetRally(), config);
+	}
+}
