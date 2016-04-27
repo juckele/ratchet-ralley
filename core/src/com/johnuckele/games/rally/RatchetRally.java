21ca8fcb775c5eca18cdb431a65387390eb66c11
John H. Uckele <j@vivarium.io>
2016-04-27 19:39:18 -0400

    Removed some shapes

--- core/src/com/johnuckele/games/rally/RatchetRally.java
+++ core/src/com/johnuckele/games/rally/RatchetRally.java
@@ -167,25 +167,12 @@ public class RatchetRally implements ApplicationListener {
 		mb.node().id = "box";
 		mb.part("box", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.BLUE)))
 			.box(1f, 1f, 1f);
-		mb.node().id = "cone";
-		mb.part("cone", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.YELLOW)))
-			.cone(1f, 2f, 1f, 10);
-		mb.node().id = "capsule";
-		mb.part("capsule", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal, new Material(ColorAttribute.createDiffuse(Color.CYAN)))
-			.capsule(0.5f, 2f, 10);
-		mb.node().id = "cylinder";
-		mb.part("cylinder", GL20.GL_TRIANGLES, Usage.Position | Usage.Normal,
-			new Material(ColorAttribute.createDiffuse(Color.MAGENTA))).cylinder(1f, 2f, 1f, 10);
 		model = mb.end();
 
 		constructors = new ArrayMap<String, GameObject.Constructor>(String.class, GameObject.Constructor.class);
 		constructors.put("ground", new GameObject.Constructor(model, "ground", new btBoxShape(new Vector3(25f, 0.5f, 25f)), 0f));
 		constructors.put("sphere", new GameObject.Constructor(model, "sphere", new btSphereShape(0.5f), 1f));
 		constructors.put("box", new GameObject.Constructor(model, "box", new btBoxShape(new Vector3(0.5f, 0.5f, 0.5f)), 1f));
-		constructors.put("cone", new GameObject.Constructor(model, "cone", new btConeShape(0.5f, 2f), 1f));
-		constructors.put("capsule", new GameObject.Constructor(model, "capsule", new btCapsuleShape(.5f, 1f), 1f));
-		constructors.put("cylinder", new GameObject.Constructor(model, "cylinder", new btCylinderShape(new Vector3(.5f, 1f, .5f)),
-			1f));
 
 		collisionConfig = new btDefaultCollisionConfiguration();
 		dispatcher = new btCollisionDispatcher(collisionConfig);
